b6fd94a354ff75a7a8856c96de7566fc5b40654b
Zeke Farwell <zeke@gearx.com>
2013-12-09 23:34:30 -0500

    updated to version 2.5.9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 # This is a placeholder class.
-class forklift($version='2.5.6') {
+class forklift($version='2.5.9') {
   package { 'Forklift':
     provider => 'compressed_app',
     source   => "http://download.binarynights.com/ForkLift${version}.zip"
--- spec/classes/forklift_spec.rb
+++ spec/classes/forklift_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'forklift' do
 
-  version = '2.5.6'
+  version = '2.5.9'
 
   it do
     should contain_package('Forklift').with({
